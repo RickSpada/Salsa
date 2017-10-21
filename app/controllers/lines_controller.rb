3809c2aa71d1dd7cb2979720e258d9575cbe3088
Rick Spada <rspada@yesware.com>
2017-10-21 16:18:54 -0400

    initial commit of lines controller

---
+++ app/controllers/lines_controller.rb
@@ -0,0 +1,6 @@
+class LinesController < ApplicationController
+  # GET /lines
+  def index
+    byebug
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+Salsa::Application.routes.draw do
+  # the 'lines' endpoint, define only for 'index'
+  resources :lines, only: [:index]
 end
