675dcfeee653121e9a94da76b723f6a09b4ba4c9
Rick Spada <rspada@yesware.com>
2017-10-22 11:28:54 -0400

    skip authentication

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -1,6 +1,8 @@
 class LinesController < ApplicationController
+  # maybe someday add authentication
+  skip_before_action :verify_authenticity_token
+
   # GET /lines
   def index
-    byebug
   end
 end
