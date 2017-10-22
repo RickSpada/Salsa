c42edb34156589e6e20980f389f5883379c59130
Rick Spada <rspada@yesware.com>
2017-10-22 15:10:55 -0400

    call the Get method to get a specific line

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -2,7 +2,21 @@ class LinesController < ApplicationController
   # maybe someday add authentication
   skip_before_action :verify_authenticity_token
 
+  # TODO DRY this up
+  rescue_from ActionController::ParameterMissing do |e|
+    render :json => { :errors => e.message }, :status => :bad_request
+  end
+
+  rescue_from StandardError do |e|
+    render :json => { :errors => e.message }, :status => 413
+  end
+
   # GET /lines
   def index
+    params.require(:line_number)
+
+    # Fetch the requested line number
+    line = Lines::Get.call(params[:line_number])
+    render({ :json => { line: line }, :root => true, :status => 200 })
   end
 end
