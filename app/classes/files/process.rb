f48518b13e56c60bb11218b8cac4670787b880b4
Rick Spada <rspada@yesware.com>
2017-10-22 12:33:36 -0400

    Update the singleton with the new file and array of lines.

--- app/classes/files/process.rb
+++ app/classes/files/process.rb
@@ -8,11 +8,18 @@ class Files::Process
   end
 
   def call
-    puts @file_name
+    raise StandardError, "File, '#{@file_name}' not found." if not File.file?(@file_name)
 
+    # get the SalsaFile singleton and reset it
     salsa = SalsaFile.instance
-    salsa.file_name = @file_name
+    salsa.reset
 
-    puts salsa.file_name
+    # read in each line into the singleton's 'lines' attribute
+    lines = []
+    File.open(@file_name, 'r').each { |line| lines << line }
+    salsa.update_attributes(file_name: @file_name, lines: lines)
+
+    # return the number of lines
+    salsa.lines.length
   end
 end
